8c74683fcd7fb314403b916a866368836403de1d
Holger Veltrup <veltrup@sitepark.com>
2025-03-18 11:18:46 +0100

    refactore: remove unused 'use'

--- src/Service/UrlRewriteHandlerCollection.php
+++ src/Service/UrlRewriteHandlerCollection.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Atoolo\Rewrite\Service;
 
 use Atoolo\Rewrite\Dto\Url;
-use Atoolo\Rewrite\Dto\UrlBuilder;
 use Atoolo\Rewrite\Dto\UrlRewriteOptions;
 use Atoolo\Rewrite\Dto\UrlRewriterHandlerContext;
 use Atoolo\Rewrite\Dto\UrlRewriteType;
